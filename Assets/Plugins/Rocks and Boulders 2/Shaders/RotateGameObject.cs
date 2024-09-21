2e93eb51920b5d6476d1c3f50e960e1a5934352f
AlejandroDVDSM <bysalexx@gmail.com>
2024-09-21 12:09:57 +0100

    Track all files under Assets/Plugins folder

--- Assets/Plugins/Rocks and Boulders 2/Shaders/RotateGameObject.cs
+++ Assets/Plugins/Rocks and Boulders 2/Shaders/RotateGameObject.cs
@@ -1,23 +1,3 @@
-using UnityEngine;
-using System.Collections;
-
-public class RotateGameObject : MonoBehaviour {
-	public float rot_speed_x=0;
-	public float rot_speed_y=0;
-	public float rot_speed_z=0;
-	public bool local=false;
-	
-	// Use this for initialization
-	void Start () { 
-	
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		if (local) {
-			transform.RotateAroundLocal(transform.up, Time.fixedDeltaTime*rot_speed_x);
-		} else {
-			transform.Rotate(Time.fixedDeltaTime*new Vector3(rot_speed_x,rot_speed_y,rot_speed_z), Space.World);
-		}
-	}
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:f0db18c7bf107f31891dfcae66dc148e5734083b08711eb55fe03d75db949fe6
+size 543
